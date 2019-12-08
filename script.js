f520f810daa12e89465ef67420c0b269f5cb42fd
te26Oku <teturou@aw.wakwak.com>
2019-12-08 21:57:53 +0900

    add click event

--- script.js
+++ script.js
@@ -120,31 +120,31 @@ function renderPlaces(places) {
         //     setModel(models[newIndex], entity);
         // });
 
-        // const clickListener = function (ev) {
-        //     ev.stopPropagation();
-        //     ev.preventDefault();
+        const clickListener = function (ev) {
+            ev.stopPropagation();
+            ev.preventDefault();
 
-        //     const name = ev.target.getAttribute('name');
+            const name = ev.target.getAttribute('name');
 
-        //     const el = ev.detail.intersection && ev.detail.intersection.object.el;
+            const el = ev.detail.intersection && ev.detail.intersection.object.el;
 
-        //     if (el && el === ev.target) {
-        //         // const label = document.createElement('span');
-        //         // const container = document.createElement('div');
-        //         const label = document.querySelector('.instructions');
+            if (el && el === ev.target) {
+                // const label = document.createElement('span');
+                // const container = document.createElement('div');
+                const label = document.querySelector('.instructions');
                 
-        //         // container.setAttribute('id', 'place-label');
-        //         label.innerText = name;
-        //         // container.appendChild(label);
-        //         // document.body.appendChild(container);
+                // container.setAttribute('id', 'place-label');
+                label.innerText = name;
+                // container.appendChild(label);
+                // document.body.appendChild(container);
 
-        //         setTimeout(() => {
-        //             container.parentElement.removeChild(container);
-        //         }, 1500);
-        //     }
-        // };
+                setTimeout(() => {
+                    container.parentElement.removeChild(container);
+                }, 1500);
+            }
+        };
 
-        // model.addEventListener('click', clickListener);
+        model.addEventListener('click', clickListener);
 
         scene.appendChild(model);
     });
