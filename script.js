8e620f6ac442d60569a98f9afe07ac3c26a66f7e
te26Oku <teturou@aw.wakwak.com>
2019-12-10 19:12:14 +0900

    add build number

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.onload = () => {
     const button = document.querySelector('button[data-action="change"]');
     // button.innerText = '+';
-    button.innerText = '4';
+    button.innerText = '5';
 
     // let places = staticLoadPlaces();
 
